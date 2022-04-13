63c5afc578246dc1decf3626f5c07cc41e2ce0d0
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-04-13 15:20:25 +0000

    генерация 2.0(вернул удаленное)

--- askme/app/management/commands/gen.py
+++ askme/app/management/commands/gen.py
@@ -24,7 +24,34 @@ class Command(BaseCommand):
                 text = text + rand_string + ' '
             return text
 
-#
+    # generate profiles
+    for i in range(user_numbers):
+        user = User(username= generate_text(1,1,random.randrange(4, 15, 1)), password=generate_text(1,1,random.randrange(10, 15, 1)), is_staff=False, is_active=True, is_superuser=False)
+        user.save()
+        profile = Profile(user=user, avatar=random.choice(avatars_set))
+        profile.save()
+    print("profiles created")
+    #generate tags
+    uses_tag = []
+    for i in range(tags_numbers):
+        teg = generate_text(1,1, random.randrange(3, 6, 1))
+        while teg in uses_tag:
+            teg = generate_text(1,1, random.randrange(3, 6, 1))    
+        uses_tag.append(teg)
+        tag = Tag(tag = teg, count = random.randrange(0, 1000, 1))
+        tag.save()
+    print("tags created")
+    #generate questions
+    for i in range(questions_numbers):
+        title = generate_text(3, 10,random.randrange(3, 10, 1))
+        text = generate_text(20, 70,random.randrange(3, 10, 1))
+        author = random.choice(User.objects.all())
+        question = Question(title=title, text=text, author=author, likes = random.randrange(1, 10000, 1))
+        question.save()
+        count_tags = random.randrange(1, 5)
+        for k in range(count_tags):
+            question.tag.add(random.choice(Tag.tags.all()))
+    print("questions created")
     #generate answers
     for i in range(answers_numbers):
         text = generate_text(30,200,random.randrange(3, 10, 1))
